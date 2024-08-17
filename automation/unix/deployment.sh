bad0b96d3de222a280013a12473c6da8b1fedf61
Lex van Os <vanoslex7@gmail.com>
2024-08-17 16:57:14 +0200

    Shell script improvements

--- automation/unix/deployment.sh
+++ automation/unix/deployment.sh
@@ -3,6 +3,16 @@
 # Variables for the provisioning and configuration of the web application environment
 KEY_NAME="my-key-pair"
 
+# Function to handle errors and exit the script
+handle_error() {
+    echo "An error occurred. Exiting..."
+    terraform destroy -auto-approve
+    exit 1
+}
+
+# Set the error handler
+trap handle_error ERR
+
 # Step 1: Run Terraform to provision the web application environment
 echo "Initializing Terraform..."
 cd terraform/aws-web-app
@@ -13,18 +23,16 @@ terraform apply -auto-approve
 
 echo "Provisioning complete."
 
-# Step 2: Export of the private key to the local machine
+# Step 2: Export the private key to the local machine
 echo "Exporting the private key..."
-
-
 terraform output -raw private_key_pem > keys/web_server_key.pem
+
+echo "Setting the permissions for the private key..."
 chmod 400 keys/web_server_key.pem
 
 echo "Private key exported."
 
 # Step 3: Run Ansible to configure the web application environment
-
 echo "Configuring the web application environment..."
-
 cd ../../ansible
-ansible-playbook -i inventory/hosts playbook.yml 
+ansible-playbook -i inventory/hosts playbook.yml
